4453a8035de38e61a6be40e580940b3cba27dc1d
COANelmejor <coanelmejor@gmail.com>
2016-07-17 00:02:50 -0600

    Iniciada la página de Inicio, y agregado el json para los botones del navde todo el sitio.

---
+++ app/appNav.js
@@ -0,0 +1,29 @@
+var botones = [
+{
+	"name":"Inicio",
+	"link":"index.html"
+},{
+	"name":"Cómo Funciona",
+	"link":"comoFunciona.html"
+},{
+	"name":"Costos",
+	"link":"costos.html"
+},{
+	"name":"Graduaciones USDE",
+	"link":"graduacionesUSDE.html"
+},{
+	"name":"plan de Estudios",
+	"link":"planDeEstudios.html"
+},{
+	"name":"Escuela de Idiomas",
+	"link":"escuelaDeIdiomas.html"
+},{
+	"name":"Otras Tutorías",
+	"link":"otrasTutorias.html"
+}.{
+	"name":"Contácto",
+	"link":"contacto.html"
+}
+]
+
+var 
