312efe025358ecbf448e72d67c80d336b4663d84
COANelmejor <coanelmejor@gmail.com>
2016-07-16 19:04:16 -0600

    Agregado a app/ appInicio.js y appPlanDeEstudios.js para integrarse a la pagina web

---
+++ app/appInicio.js
@@ -0,0 +1,64 @@
+var sec1 = {
+	"titulo":"Su mejor elección para la educación de sus hijos.",
+	"image":"Niña escribiendo en el suelo. Fondo"
+};
+
+var sec2 = {
+	"titulo":"Educación en Casa",	
+	"contenido":[
+		"Somos la más reciente y moderna opción en el área pedagógica y tecnológica que nos permite desarrollar programas de home schooling adaptados a las necesidades y exigencias de la actualidad.",
+		"La mayoría de los colegios Guatemaltecos solo ofrecen educación aprobada por el Ministerio de Educación, pero el diploma que se obtiene no les permite a los estudiantes entradas a Universidades de USA.",
+		"Los programas de WELDS ofrecen estudios desde primer grado hasta doce grado, de acuerdo a estándares de USA., lo cual le otorga al alumno egresado reconocimientos educativos o diplomas de graduación, de un colegio americano aprobado por US Department of Education, lo que les da acceso muchas universidades americanas."
+	],
+	"image":"Adolescente mujer sentada en el suelo. Fondo"
+};
+
+var sec3 = {
+	"lista":[
+		"Graduación en reconocida por el US Departmet of Education",
+		"Entrar a Universidades Americanas",
+		"Cursos en Ingles",
+		"Plataforma educativa on line con acceso a lecciones exámenes, investigaciones y más"
+	],
+	"images":[
+		"Gorro de graduación. Flat",
+		"Bombilla. Flat",
+		"Libro. Flat",
+		"Lapiz. Flat"
+	]
+};
+
+var sec4 = {
+	"titulo": "La llave para el futuro de sus hijos",
+	"articulos": [{
+		"titulo": "E-Books y Lecciones",
+		"image": "Grupo de adolescentes estudiando en grupo.",
+		"contenido": [
+			"Cada curso cuenta con un grupo de libros digitales y lecciones para que los alumnos puedan estudiar, estos mismos están diseñados para que los alumnos puedan ir aprendiendo a su paso."
+		]
+	}, {
+		"titulo": "Reporte de Avances",
+		"image": "Adolescente mujer estudiando en la biblioteca.",
+		"contenido": [
+			"Podrá observar en tiempo real los avances del alumno, lecciones aprendidas, punteos y cursos superados."
+		]
+
+	}, {
+
+		"titulo": "Evaluaciones Periódicas",
+		"image": "Mujer adulta maestra.",
+		"contenido": [
+			"Contamos con evaluaciones para medir el desempeño y lo aprendido para los alumnos."
+			]
+		}
+	]
+};
+var sec5 = {
+	"titulo": "Enseñanza con Valores",
+	"contenido": [
+		"Nuestros planes educativos contienes dolidos valores para que los alumnos puedan desempeñarse en un mundo laboral activo."
+	]
+};
+
+
+
---
+++ app/appPlanDeEstudios.js
@@ -0,0 +1,213 @@
+var gradoApp = 
+[
+	{ // Grado
+		"grado": 1,
+		"materias":
+		[
+			{	//Materia
+				"nombre": "Language Arts",
+				"temas":
+				[ // Temas de Materia
+					"Listening skills and beginning reading",
+					"Fairy tales",
+					"Nursery rhymes",
+					"Myths",
+					"Riddles",
+					"Aesop’s Fables",
+					"Simple sentence writing",
+					"Introduction to punctuation signs"
+				]
+			},
+			{
+				"nombre": "Math",
+				"temas":
+				[
+					"Patterns and classifications",
+					"Understanding numbers to 100",
+					"Ordinal numbers",
+					"Place Value",
+					"Using Graphs",
+					"Fractions",
+					"Adding and subtracting simple problems"
+				]
+			},
+			{
+				"nombre":"Science",
+				"temas":
+				[
+					"Living Things and Their Habitats: Deserts &oceans",
+					"The Food Chain",
+					"Dinosaurs",
+					"Introduction to the body",
+					"Matter",
+					"Units of Measurement",
+					"The Environment",
+					"Plants",
+					"Animals, ",
+					"The solar system",
+					"Measuring the seasons."
+				]
+			},
+			{
+				"nombre":"Social Science",
+				"temas":
+				[
+					"An introduction to the Ice Age,",
+					"Egypt",
+					"Mesopotamia",
+					"Hammurabi",
+					"Crossing the Land Bridge",
+					"Early American history",
+					"World religions"
+				]
+			},
+			{
+				"nombre":"Fine Arts",
+
+			}
+		]	
+	},
+	{ 
+		"grado": 2,
+		"materias":
+		[ 
+			{ //Materia
+				"nombre": "",
+				"temas":
+				[
+					// Temas de Materia
+				]
+			}
+		]
+	},
+	{ 
+		"grado": 3,
+		"materias":
+		[ 
+			{ //Materia
+				"nombre": "",
+				"temas":
+				[
+					// Temas de Materia
+				]
+			}
+		]
+	},
+	{ 
+		"grado": 4,
+		"materias":
+		[ 
+			{ //Materia
+				"nombre": "",
+				"temas":
+				[
+					// Temas de Materia
+				]
+			}
+		]
+	},
+	{ 
+		"grado": 5,
+		"materias":
+		[ 
+			{ //Materia
+				"nombre": "",
+				"temas":
+				[
+					// Temas de Materia
+				]
+			}
+		]
+	},
+	{ 
+		"grado": 6,
+		"materias":
+		[ 
+			{ //Materia
+				"nombre": "",
+				"temas":
+				[
+					// Temas de Materia
+				]
+			}
+		]
+	},
+	{ 
+		"grado": 7,
+		"materias":
+		[ 
+			{ //Materia
+				"nombre": "",
+				"temas":
+				[
+					// Temas de Materia
+				]
+			}
+		]
+	},
+	{ 
+		"grado": 8,
+		"materias":
+		[ 
+			{ //Materia
+				"nombre": "",
+				"temas":
+				[
+					// Temas de Materia
+				]
+			}
+		]
+	},
+	{ 
+		"grado": 9,
+		"materias":
+		[ 
+			{ //Materia
+				"nombre": "",
+				"temas":
+				[
+					// Temas de Materia
+				]
+			}
+		]
+	},
+	{ 
+		"grado": 10,
+		"materias":
+		[ 
+			{ //Materia
+				"nombre": "",
+				"temas":
+				[
+					// Temas de Materia
+				]
+			}
+		]
+	},
+	{ 
+		"grado": 11,
+		"materias":
+		[ 
+			{ //Materia
+				"nombre": "",
+				"temas":
+				[
+					// Temas de Materia
+				]
+			}
+		]
+	},
+	{ 
+		"grado": 12,
+		"materias":
+		[ 
+			{ //Materia
+				"nombre": "",
+				"temas":
+				[
+					// Temas de Materia
+				]
+			}
+		]
+	}
+]
