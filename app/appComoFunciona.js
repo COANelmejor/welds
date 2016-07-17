2160058d219dbcc71622eec70d021721db4d68a8
Uriel Arana <udan1718@gmail.com>
2016-07-16 18:55:38 -0600

    Creación de cd app, Y AppComo Funciona.js

---
+++ app/appComoFunciona.js
@@ -0,0 +1,42 @@
+var sec1 = {
+	"titulo": "Como Funciona?",
+	"sub":"¿Como Me Inscribo?",
+	"contenido":"Los Pasos que debes seguir son los siguientes: ",
+	"lista":[
+		"Ponte en contacto a través de nuestra página y se solicitara algunos documentos necesarios para la inscripción.",
+		"Debes realizar un examen de ubicación, el cual podrá ser en línea, cualquier día y cualquier hora o en nuestras instalaciones, previa cita de día y horario disponible.",
+		"El examen de ubicación tiene un costo de $75.00.",
+		"Luego se te enviará vía correo electrónico todos los pasos para necesarios para la inscripción, pago de cuotas, entrega de materiales y la adjudicación de usuario dentro de la plataforma educativa.",
+		"Listo, puedes empezar a estudiar a tu ritmo, con tu horario y con disponibilidad de tutorías."
+	]};
+
+var sec2 = {
+	"titulo":"¿Cómo funciona la plataforma educativa?",
+	"lista":[
+		"El alumno recibe con la inscripción todo el material que va a utilizar.",
+		"Si tiene dudas puede, por medio de la plataforma solicitar tutorías, de lunes a viernes, en horario de 8 am a 5 pm un máximo de 1 hora diaria, con tutor en forma personal.",
+		"Por medio de la plataforma recibirás las tutorías y más material para desarrollar tus conocimientos, podrás también participar el foros y trabajo en grupo para interactuar, desde casa, con la comunidad educativa.",
+		"Recibirás informes y evaluaciones para conocer avances de lo aprendido.",
+		"Tienes acceso desde cualquier dispositivo con acceso a internet, 24/7 (no aplica a tutorías)."
+	],
+};
+
+var sec3 = {
+	"titulo":"¿Cómo obtengo mis documentos de grados aprobados?",
+	"contnido":"Por medio de nuestra plataforma se mantendrá la información permanente de los avances del estudiante y cuando este complete un grado en particular, será notificado y se le entregará sus respectivas constancias de aprobación."
+};
+
+var sec4 = {
+	"titulo":"¿En qué momento me puedo integrar al programa de estudio?",
+	"contenido":""
+};
+
+var sec5 = {
+	"titulo":"",
+	"contenido":""
+};
+
+var sec6 = {
+	"titulo":"",
+	"contenido":""
+};
