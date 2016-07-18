81bfc2ac3e713fcf78104aecd8a4002d9c248fdb
COANelmejor <coanelmejor@gmail.com>
2016-07-17 23:26:44 -0600

    Terminado comoFunciona.html, y modificado algunos datos de appNav.js

--- app/appNav.js
+++ app/appNav.js
@@ -126,51 +126,58 @@ var app = angular.module('appNav', [ ]);
 
 	//Contenido de comoFunciona.html
 	app.controller('contentComoFunciona', function(){
-		this.contenido = fucniona;
+		this.contenido = funciona;
 	})
 
-	var funciona = [
-	{
-		"titulo": "Como Funciona?",
-		"sub":"¿Como Me Inscribo?",
-		"contenido":["Los Pasos que debes seguir son los siguientes: "],
-		"lista":[
-			"Ponte en contacto a través de nuestra página y se solicitara algunos documentos necesarios para la inscripción.",
-			"Debes realizar un examen de ubicación, el cual podrá ser en línea, cualquier día y cualquier hora o en nuestras instalaciones, previa cita de día y horario disponible.",
-			"El examen de ubicación tiene un costo de $75.00.",
-			"Luego se te enviará vía correo electrónico todos los pasos para necesarios para la inscripción, pago de cuotas, entrega de materiales y la adjudicación de usuario dentro de la plataforma educativa.",
-			"Listo, puedes empezar a estudiar a tu ritmo, con tu horario y con disponibilidad de tutorías."
-		]
-	},{
-		"titulo":"¿Cómo funciona la plataforma educativa?",
-		"lista":[
-			"El alumno recibe con la inscripción todo el material que va a utilizar.",
-			"Si tiene dudas puede, por medio de la plataforma solicitar tutorías, de lunes a viernes, en horario de 8 am a 5 pm un máximo de 1 hora diaria, con tutor en forma personal.",
-			"Por medio de la plataforma recibirás las tutorías y más material para desarrollar tus conocimientos, podrás también participar el foros y trabajo en grupo para interactuar, desde casa, con la comunidad educativa.",
-			"Recibirás informes y evaluaciones para conocer avances de lo aprendido.",
-			"Tienes acceso desde cualquier dispositivo con acceso a internet, 24/7 (no aplica a tutorías)."
-		],
-	},{
-		"titulo":"¿Cómo obtengo mis documentos de grados aprobados?",
+	var funciona = {
+		"titulo": "¿Como Funciona Welds?",
 		"contenido":
 		[
-			"Por medio de nuestra plataforma se mantendrá la información permanente de los avances del estudiante y cuando este complete un grado en particular, será notificado y se le entregará sus respectivas constancias de aprobación."
+			{
+				"titulo":"¿Como Me Inscribo?",
+				"contenido":
+				[
+					"Los Pasos que debes seguir son los siguientes:"
+				],
+				"lista":
+				[
+					"Ponte en contacto a través de nuestra página y se solicitara algunos documentos necesarios para la inscripción.",
+					"Debes realizar un examen de ubicación, el cual podrá ser en línea, cualquier día y cualquier hora o en nuestras instalaciones, previa cita de día y horario disponible.",
+					"El examen de ubicación tiene un costo de $75.00.",
+					"Luego se te enviará vía correo electrónico todos los pasos para necesarios para la inscripción, pago de cuotas, entrega de materiales y la adjudicación de usuario dentro de la plataforma educativa.",
+					"Listo, puedes empezar a estudiar a tu ritmo, con tu horario y con disponibilidad de tutorías."
+				]
+			},{
+				"titulo":"¿Cómo funciona la plataforma educativa?",
+				"lista":[
+					"El alumno recibe con la inscripción todo el material que va a utilizar.",
+					"Si tiene dudas puede, por medio de la plataforma solicitar tutorías, de lunes a viernes, en horario de 8 am a 5 pm un máximo de 1 hora diaria, con tutor en forma personal.",
+					"Por medio de la plataforma recibirás las tutorías y más material para desarrollar tus conocimientos, podrás también participar el foros y trabajo en grupo para interactuar, desde casa, con la comunidad educativa.",
+					"Recibirás informes y evaluaciones para conocer avances de lo aprendido.",
+					"Tienes acceso desde cualquier dispositivo con acceso a internet, 24/7 (no aplica a tutorías)."
+			],
+			},{
+				"titulo":"¿Cómo obtengo mis documentos de grados aprobados?",
+				"contenido":
+				[
+					"Por medio de nuestra plataforma se mantendrá la información permanente de los avances del estudiante y cuando este complete un grado en particular, será notificado y se le entregará sus respectivas constancias de aprobación."
+				]
+			},{
+				"titulo":"¿En qué momento me puedo integrar al programa de estudio?",
+				"contenido":
+				[
+					"En cualquier momento, dada su naturaleza de principio, un alumno puede integrarse en el momento que lo desee y comenzar sus estudios."
+				]
+			},{
+				"titulo":"¿Hay algún costo adicional?",
+				"contenido":
+				[
+					"Únicamente en el momento que el alumno se gradúa tiene un costo adicional de $60.00 por los trámites de acreditación consular del título.",
+					"Queda también pendientes, los costos del acto de graduación a llevarse a cabo en el país."
+				]
+			}
 		]
-	},{
-		"titulo":"¿En qué momento me puedo integrar al programa de estudio?",
-		"contenido":
-		[
-			"En cualquier momento, dada su naturaleza de principio, un alumno puede integrarse en el momento que lo desee y comenzar sus estudios."
-		]
-	},{
-		"titulo":"¿Hay algún costo adicional?",
-		"contenido":
-		[
-			"Únicamente en el momento que el alumno se gradúa tiene un costo adicional de $60.00 por los trámites de acreditación consular del título.",
-			"Queda también pendientes, los costos del acto de graduación a llevarse a cabo en el país."
-		]
-		}
-	];
+	};
 
 	//Contenido de planDeEstudios.html
 	app.controller('contentGrado', function(){
